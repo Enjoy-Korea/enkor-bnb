b319ae080ae85c7644574ae3b68488065c3d101a
heejin99 <wkdgmlwls30@naver.com>
2021-11-15 03:10:10 +0900

    Feat: Error 처리 추가
    - app.json에 Error 처리 추가

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
 const { jwtMiddleware } = require("./libs/token");
 const cors = require("cors");
 const router = require("./router");
@@ -19,8 +18,28 @@ const port = process.env.port || 5000;
 app.use(cors({ credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser());
 app.use(jwtMiddleware);
 app.use("/", router);
-
+app.use((err, req, res, next) => {
+	if (err.message === "Unauthorized") err.status = 401;
+	else if (err.message === "Forbidden") err.status = 403;
+	switch (err.status) {
+		case 401:
+			if (!err.message) err.message = "Unauthorized";
+			break;
+		case 403:
+			if (!err.message) err.message = "Forbidden";
+			break;
+		case 404:
+			if (!err.message) err.message = "not found";
+			break;
+		case 409:
+			if (!err.message) err.message = "Duplicated";
+			break;
+		default:
+			err.status = 500;
+			err.message = "서버에서 문제가 발생했습니다.";
+	}
+	res.status(err.status).send({ message: err.message });
+});
 app.listen(port, () => console.log(`Server listening on port ${port}`));
