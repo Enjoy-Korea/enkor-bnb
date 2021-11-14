1a56b729fb571b691716e4867285c7eca4642a09
heejin99 <wkdgmlwls30@naver.com>
2021-11-15 03:11:07 +0900

    Test: Unit Test 구현
    - userController Unit Test 구현
    - roomController Unit Test 구현
    - reservationController Unit Test 구현
    - Jest 설정 추가 (unit test를 위해)

---
+++ tests/reservationController.test.js
@@ -0,0 +1,83 @@
+const Reservation = require("../controllers/reservationController");
+
+const res = {
+	status: jest.fn(() => res),
+	json: jest.fn()
+};
+const next = jest.fn();
+
+const mockReservation = {
+	findOne: jest.fn(),
+	update: jest.fn(),
+	create: jest.fn()
+};
+
+const reservation = {
+	query: {
+		id: 1
+	},
+	user: {
+		id: 1
+	}
+};
+
+const reservationInfo = {
+	params: {
+		id: 1
+	},
+	user: {
+		id: 1,
+		email: "test@test.com"
+	}
+};
+
+const failInfo = {
+	params: {
+		id: 200
+	},
+	user: {
+		id: 1,
+		email: "test@test.com"
+	}
+};
+const newReservation = {
+	id: 1,
+	roomId: "2",
+	reservationType: true,
+	userId: 10,
+	updatedAt: "2021-11-14T14:32:11.715Z",
+	createdAt: "2021-11-14T14:32:11.715Z"
+};
+
+describe("Room Test", () => {
+	it("Reservation Book Success", async () => {
+		// given
+		mockReservation.findOne.mockReturnValue(newReservation);
+
+		// when
+		await Reservation.book(reservation, res, next);
+
+		// then
+		expect(res.status).toBeCalledWith(201);
+	});
+	it("Reservation Info Success", async () => {
+		// given
+		mockReservation.findOne.mockReturnValue(newReservation);
+		// when
+		await Reservation.reservationInfo(reservationInfo, res, next);
+		// then
+		expect(res.status).toBeCalledWith(200);
+	});
+
+	it("Reservation Info Fail (reservationId 없음)", async () => {
+		// given
+		mockReservation.findOne.mockReturnValue(null);
+		try {
+			// when
+			await Reservation.reservationInfo(failInfo, res, next);
+		} catch (err) {
+			// then
+			expect(res.status).toBeCalledWith(404);
+		}
+	});
+});
---
+++ tests/userController.test.js
@@ -0,0 +1,107 @@
+const User = require("../controllers/userController");
+
+const token = "tokenTest";
+const user = {
+	body: {
+		userEmail: "test8@test.com",
+		userPw: "test",
+		userName: "test"
+	}
+};
+
+const res = {
+	status: jest.fn(() => res),
+	json: jest.fn()
+};
+const next = jest.fn();
+const mockUser = {
+	create: jest.fn(),
+	findOne: jest.fn(),
+	checkLogin: jest.fn()
+};
+
+const newUser = {
+	body: {
+		userEmail: "new@new.com",
+		userPw: "new"
+	}
+};
+
+const failPw = {
+	body: {
+		userEmail: "test8@test.com",
+		userPw: "fail"
+	}
+};
+const mockAuth = {
+	createToken: jest.fn()
+};
+beforeAll(async () => {});
+
+describe("User Signup Test", () => {
+	it("Signup Success", async () => {
+		// given
+		mockUser.findOne.mockReturnValue(null);
+		mockUser.create.mockReturnValue(user.body);
+		mockAuth.createToken.mockReturnValue(token);
+		// when
+		await User.signup(user, res, next);
+
+		// then
+		expect(res.status).toBeCalledWith(201);
+	});
+	it("Signup Fail (Email 중복)", async () => {
+		// given
+		mockUser.findOne.mockReturnValue(user.body);
+		mockUser.create.mockReturnValue(user.body);
+		try {
+			// when
+			await User.signup(user, res, next);
+		} catch (err) {
+			// then
+			expect(err).toBeCalledWith(404);
+		}
+	});
+});
+
+describe("Signin Test", () => {
+	it("Signin Success", async () => {
+		// given
+		mockUser.checkLogin.mockReturnValue(user.body);
+		mockUser.findOne.mockReturnValue(user.body);
+
+		// when
+		await User.signin(user, res, next);
+
+		// then
+		expect(res.status).toBeCalledWith(200);
+	});
+
+	it("Signin Fail (Email 없음)", async () => {
+		// given
+		mockUser.checkLogin.mockReturnValue(newUser.body);
+		mockUser.findOne.mockReturnValue(newUser.body);
+
+		try {
+			// when
+			await User.signin(newUser, res, next);
+		} catch (err) {
+			// then
+			expect(err).toBeCalledWith(404);
+		}
+	});
+
+	it("Signin Fail (password 틀림)", async () => {
+		// given
+		mockUser.checkLogin.mockReturnValue(failPw.body);
+		mockUser.findOne.mockReturnValue(user.body);
+
+		try {
+			// when
+			await User.signin(failPw, res, next);
+		} catch (err) {
+			// then
+			expect(err).toBeCalledWith(404);
+		}
+	});
+});
